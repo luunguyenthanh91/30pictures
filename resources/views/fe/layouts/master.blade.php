ed40c44044e16b4f5bcddd499b3e9bc0dea485dc
Kennguyen3 <69205419+Kennguyen3@users.noreply.github.com>
2021-10-20 01:55:47 +0700

    upload

--- resources/views/fe/layouts/master.blade.php
+++ resources/views/fe/layouts/master.blade.php
@@ -1,188 +1,75 @@
-<!doctype html>
-<html class="no-js" lang="zxx">
+<!DOCTYPE html>
+<html lang="zxx" class="js">
 
-
-<!-- Mirrored from demo.hasthemes.com/theface-preview/theface-v3/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 21 Oct 2020 04:00:51 GMT -->
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Votes</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Place favicon.ico in the root directory -->
-    <link href="{{ asset('fe/assets/images/favicon.ico') }}" type="img/x-icon" rel="shortcut icon">
-    <!-- All css files are included here. -->
-    <link rel="stylesheet" href="{{ asset('fe/assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fe/assets/css/iconfont.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fe/assets/css/plugins.css') }}">
-    <link rel="stylesheet" href="{{ asset('fe/assets/css/helper.css') }}">
-    <link rel="stylesheet" href="{{ asset('fe/assets/css/style.css') }}">
-    <!-- Modernizr JS -->
-    <script src="{{ asset('fe/assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{Helper::getSetting(1)->description}}</title>
+    <meta property="og:url" content="{{@url('/')}}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{{Helper::getSetting(1)->description}}">
+    <meta property="og:description" content="{{Helper::getSetting(2)->description}}">
+    <meta property="og:image" content="{{@url(Helper::getSetting(4)->image_pc)}}">
+    <!-- <meta property="fb:app_id" content=""> -->
+
+    <meta name="description" content="This is the website description. Nice eh?">
+ 
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content="{{Helper::getSetting(1)->description}}">
+    <meta itemprop="description" content="{{Helper::getSetting(2)->description}}">
+    <meta itemprop="image" content="{{@url(Helper::getSetting(4)->image_pc)}}">
+   
+    
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{{Helper::getSetting(1)->description}}">
+    <meta name="twitter:description" content="{{Helper::getSetting(2)->description}}">
+    <meta name="twitter:image" content="{{@url(Helper::getSetting(4)->image_pc)}}">
+
+
+    <meta name="keywords" content="{{Helper::getSetting(1)->description}}">
+    <meta name="description" content="{{Helper::getSetting(2)->description}}">
+    <meta name="author" content="tivatheme">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{ asset('fe/media/favico_icon.png') }}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i%7CSource+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet" />
+    <!-- Bundle and Base CSS -->
+    <link href="{{ asset('fe/css/vendor.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('fe/css/vendor/jquery-ui.min.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('fe/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('fe/css/pc.css') }}">
+    <link rel="stylesheet" href="{{ asset('fe/css/mobile.css') }}">
+    <link rel="stylesheet" href="{{ asset('fe/css/home.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fe/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('fe/slick/slick-theme.css') }}">
 </head>
 
-<body>
+<body class="nk-body home_banner_video_full_header">
 
-    <div id="main-wrapper">
-
-      
-        @yield('content')
-
-        <!-- Modal Area Strat -->
-        <div class="modal fade quick-view-modal-container" id="quick-view-modal-container" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="col-xl-12 col-lg-12">
-                            <div class="row">
-                                <div class="col-xl-5 col-lg-6 col-md-6 mb-xxs-25 mb-xs-25 mb-sm-25">
-                                    <!-- Product Details Left -->
-                                    <div class="product-details-left">
-                                        <div class="product-details-images slider-lg-image-1 tf-element-carousel" data-slick-options='{
-                                            "slidesToShow": 1,
-                                            "slidesToScroll": 1,
-                                            "infinite": true,
-                                            "asNavFor": ".slider-thumbs-1",
-                                            "arrows": false,
-                                            "prevArrow": {"buttonClass": "slick-btn slick-prev", "iconClass": "fa fa-angle-left" },
-                                            "nextArrow": {"buttonClass": "slick-btn slick-next", "iconClass": "fa fa-angle-right" }
-                                            }'>
-                                            <div class="lg-image">
-                                                <img src="assets/images/product/large-product/l-product-1.jpg" alt="">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="assets/images/product/large-product/l-product-2.jpg" alt="">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="assets/images/product/large-product/l-product-3.jpg" alt="">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="assets/images/product/large-product/l-product-4.jpg" alt="">
-                                            </div>
-                                            <div class="lg-image">
-                                                <img src="assets/images/product/large-product/l-product-5.jpg" alt="">
-                                            </div>
-                                        </div>
-                                        <div class="product-details-thumbs slider-thumbs-1 tf-element-carousel" data-slick-options='{
-                                            "slidesToShow": 4,
-                                            "slidesToScroll": 1,
-                                            "infinite": true,
-                                            "focusOnSelect": true,
-                                            "asNavFor": ".slider-lg-image-1",
-                                            "arrows": false,
-                                            "prevArrow": {"buttonClass": "slick-btn slick-prev", "iconClass": "fa fa-angle-left" },
-                                            "nextArrow": {"buttonClass": "slick-btn slick-next", "iconClass": "fa fa-angle-right" }
-                                            }' data-slick-responsive= '[
-                                            {"breakpoint":991, "settings": {
-                                                "slidesToShow": 3
-                                            }},
-                                            {"breakpoint":767, "settings": {
-                                                "slidesToShow": 4
-                                            }},
-                                            {"breakpoint":479, "settings": {
-                                                "slidesToShow": 2
-                                            }}
-                                        ]'>										
-                                            <div class="sm-image"><img src="assets/images/product/small-product/s-product-1.jpg" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="assets/images/product/small-product/s-product-2.jpg" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="assets/images/product/small-product/s-product-3.jpg" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="assets/images/product/small-product/s-product-4.jpg" alt="product image thumb"></div>
-                                            <div class="sm-image"><img src="assets/images/product/small-product/s-product-5.jpg" alt="product image thumb"></div>
-                                        </div>
-                                    </div>
-                                    <!--Product Details Left -->
-                                </div>
-                                <div class="col-xl-7 col-lg-6 col-md-6">
-                                    <!-- product detail content -->
-                                    
-                                    <!--Product Details Content Start-->
-                                    <div class="product-details-content">
-                                        <!--Product Nav Start-->
-                                        <div class="product-nav">
-                                            <a href="#"><i class="fa fa-angle-left"></i></a>
-                                            <a href="#"><i class="fa fa-angle-right"></i></a>
-                                        </div>
-                                        <!--Product Nav End-->
-                                        <h2>White Shave Brux</h2>
-                                        <div class="single-product-reviews">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-o"></i>
-                                            <a class="review-link" href="#">(1 customer review)</a>
-                                        </div>
-                                        <div class="single-product-price">
-                                            <span class="price new-price">$66.00</span>
-                                            <span class="regular-price">$77.00</span>
-                                        </div>
-                                        <div class="product-description">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
-                                        </div>
-                                        <div class="single-product-quantity">
-                                            <form class="add-quantity" action="#">
-                                                <div class="product-quantity">
-                                                    <input value="1" type="number">
-                                                </div>
-                                                <div class="add-to-link">
-                                                    <button class="product-add-btn" data-text="add to cart">add to cart</button>
-                                                </div>
-                                            </form>
-                                    </div>
-                                        <div class="wishlist-compare-btn">
-                                            <a href="#" class="wishlist-btn mb-md-10 mb-sm-10">Add to Wishlist</a>
-                                            <a href="#" class="add-compare">Compare</a>
-                                        </div>
-                                        <div class="product-meta">
-                                            <span class="posted-in">
-                                                    Categories: 
-                                                    <a href="#">Accessories</a>,
-                                                    <a href="#">Electronics</a>
-                                                </span>
-                                        </div>
-                                        <div class="single-product-sharing">
-                                            <h3>Share this product</h3>
-                                            <ul>
-                                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                                <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <!--Product Details Content End-->
-                                    
-                                    <!-- End of product detail content -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    
-            </div>
-        </div>
-        <!-- Modal Area End -->
-
+    <div class="nk-wrap">
+        @include('fe.includes.header')
+        <main>
+            @yield('content')
+            @include('fe.layouts.menu')
+        </main>
+        @include('fe.includes.footer')
+        
     </div>
+    
 
-    <!-- Placed js at the end of the document so the pages load faster -->
-
-    <!-- All jquery file included here -->
-    <script src="{{ asset('fe/assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
-    <script src="{{ asset('fe/assets/js/popper.min.js') }}"></script>
-    <script src="{{ asset('fe/assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('fe/assets/js/plugins.js"') }}></script>
-    <script src="{{ asset('fe/assets/js/main.js') }}"></script>
+    <!-- JavaScript -->
+    <script src="{{ asset('fe/js/vendor/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('fe/js/vendor/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('fe/js/vendor.min.js') }}"></script>
+    <script src="{{ asset('fe/js/functions.js') }}"></script>
+    <script src="{{ asset('fe/slick/slick.js') }}" type="text/javascript" charset="utf-8"></script>
+    <script src="{{ asset('fe/js/home.js') }}"></script>
+    
+   
+    
 
 </body>
 
-
-<!-- Mirrored from demo.hasthemes.com/theface-preview/theface-v3/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 21 Oct 2020 04:01:00 GMT -->
 </html>
