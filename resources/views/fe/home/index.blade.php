2b7c34320ae05b15f2eca4fc9b9268bc2c512d40
Kennguyen3 <69205419+Kennguyen3@users.noreply.github.com>
2021-10-30 17:52:00 +0700

    up

--- resources/views/fe/home/index.blade.php
+++ resources/views/fe/home/index.blade.php
@@ -51,7 +51,7 @@
         $(document).ready(function() {
             setTimeout(function() {
                 window.location.href = '/index.html'; 
-            }, 7000);
+            }, 6300);
         });
     </script>
     
