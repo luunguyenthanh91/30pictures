b4fe9f6fd204d8ac00c17b7066e0097b168b6402
Kennguyen3 <69205419+Kennguyen3@users.noreply.github.com>
2021-12-03 07:26:10 +0700

    u

--- resources/views/fe/layouts/home.blade.php
+++ resources/views/fe/layouts/home.blade.php
@@ -2,6 +2,13 @@
 <html lang="zxx" class="js">
 
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KZHR68H');</script>
+<!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title')</title>
@@ -47,7 +54,10 @@
 </head>
 
 <body style="@yield('style_main')" class="nk-body @yield('class_body')">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZHR68H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div class="nk-wrap">
         @include('fe.includes.header')
         <main class="@yield('class_main')">
